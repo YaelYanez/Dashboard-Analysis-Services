d41aba09bdaae057c52994c02f7d80852b80fd50
Yael Yañez <yaelyanez112@gmail.com>
2020-05-13 18:41:37 -0500

    Charts Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './layout/app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BarChartComponent } from './components/charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './components/charts/pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BarChartComponent,
+    PieChartComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
