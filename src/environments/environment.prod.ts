09e0e29aa24643382d7035088c75ce2123b43876
Yael Yañez <yaelyanez112@gmail.com>
2020-05-19 20:25:32 -0500

    Enviroment Solved Issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API: {
+    EndPoint: {
+      Northwind: 'http://192.168.1.73/API.NorthwindCube/Northwind/v1/',
+    },
+  },
 };
