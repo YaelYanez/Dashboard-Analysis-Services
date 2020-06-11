7d2f1910ea4e084697034e2c521de238c15d1699
Yael Yañez <yaelyanez112@gmail.com>
2020-06-11 18:34:43 -0500

    api connection test

--- src/app/layout/app.component.ts
+++ src/app/layout/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cube-dashboard-Parcial2do';
+
+  async ngOnInit() {
+    const res = await axios.get('http://192.168.1.67:3000/');
+    console.log(res);
+  }
 }
