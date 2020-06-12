949eee2ef7c8cb13dbecf488074fd4a6a93ccf97
Yael Yañez <yaelyanez112@gmail.com>
2020-06-12 17:42:24 -0500

    Git Images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API: {
     EndPoint: {
-      Northwind: 'http://192.168.1.73/API.NorthwindCube/Northwind/v1/',
+      Northwind: 'http://192.168.1.76/API.NorthwindCube/Northwind/v1/',
     },
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API: {
     EndPoint: {
-      Northwind: 'http://192.168.1.73/API.NorthwindCube/Northwind/v1/',
+      Northwind: 'http://192.168.1.76/API.NorthwindCube/Northwind/v1/',
     },
   },
 };
